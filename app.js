f9cb010d88d5146d2ede8b9f49c47e524ae3006a
HugoSanche <hbmclaren@gmail.com>
2022-08-28 18:21:42 -0500

    termino modulo 15

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-
+let colores=['AZUL','blanco','verde','cafe___','MORADO'];
 let array=[];
 
+var x = require('lodash/array');
+
 const app = express();
+
+
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,11 +24,11 @@ app.use(express.static("public"));
 
 
 app.get("/",function(req, res){
-
-
-
-
+  //v2ar y=x.lowerCase(colores[0]);
+  a= "- hugo baltazar _";
+  console.log("test "+a.replace(/\s+/g,'').replace(/-/g,''));
 });
+
 
 app.post("/compose", function(req, res){
 const  newCompost={titulo:req.body.title,
@@ -35,43 +40,27 @@ const  newCompost={titulo:req.body.title,
 });
 
 
+app.get("/post/:postId", function(req, res){
+  console.log("we are working on "+req.params.postId);
+
+
+  array.forEach((item) => {
+    if (item.titulo===req.params.postId){
+      console.log("its Match");
+    }
+    else {
+        console.log("its not match");
+    }
+
+
+    //console.log(item.titulo);
+  });
+
+  // console.log("we are working on "+req.news.newsId);
+});
+
+
 app.get("/home",function(req,res){
-//   var arr_like_obj = {
-//     0: "we",
-//     1: "are",
-//     2: "capscode",
-//     length: 3,
-//   };
-// console.log(arr_like_obj);
-// console.log(array);
-
-
-// array = JSON.parse(JSON.stringify(array));
-// console.log(array.titulo);
-//
-
- // var object = array.reduce(
- //   (obj, item) => Object.assign(obj, { [item.titulo]: item.contenido }), {});
- // console.log(object);
-
-  //Object.values(array).forEach(item => {string2 += item});
-  // console.log(typeof array);
-   //console.log(array.contenido);
-
-     // array.forEach((item) => {
-     //   console.log("El valor de "+item.titulo);
-     // });
-
-//console.log(array[0].titulo);
-
- //console.log(array);
-// for (i=0;i<array.length; i++)
-//   {
-//     //console.log(i);
-//     console.log(array[i].titulo);
-//    }
-
-
 // //array = JSON.parse(JSON.stringify(array));
    res.render("home",{textoHome:homeStartingContent,newTexto:array
 //
@@ -93,12 +82,8 @@ app.get("/contact",function(req, res){
 
 app.get("/compose",function(req, res){
   // res.render("home");
-
   res.render("compose");
-
 });
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
