f6345d183299b234597c794f37a9f45fb6737f29
HugoSanche <hbmclaren@gmail.com>
2022-08-30 14:54:55 -0500

    Ultimo cambio

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 //jshint esversion:6
+
+//Este proyecto tiene como objetivo crear un diario. Aqui se van guardando el historial de las tareas realizadas
+//De igual manera se tiene como objetivo el uso de plantillas
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -7,6 +11,9 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+//En esta variable se guardan todas las tareas
+//title: el titulo de la tareas
+//contenido: aqui va todo el detalle de la tarea
 let array=[];
 
 let postWithOutSpaces;
@@ -15,14 +22,19 @@ let webPostWithOutSpaces;
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+//Permite los templates
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//Cuando se llama a raiz se redirecciona "/home"
 app.get("/",function(req, res){
 res.redirect("/home");
 });
 
+//aqui se van agregando todas las tareas
 app.post("/compose", function(req, res){
 const  newCompost={titulo:req.body.title,
     contenido:req.body.post
@@ -31,27 +43,29 @@ const  newCompost={titulo:req.body.title,
   res.redirect("/home");
 });
 
+//En base al titulo se manda a llamar el post.
+//postId permite realizar llamados en forma dinamica.Toma como base el titulo al cual le quita espacios y los sustituye
+//por "-". De igual manera cambia el texto de mayusculas a minusculas.
 app.get("/post/:postId", function(req, res){
     let tituloId;
     let titulo;
 
     array.forEach((item) => {
     titulo=item.titulo;
-    titulo=item.titulo.replace(/\s+/g,'-');  //sustituimos espacios en blanco to "-""
-    titulo=item.titulo.toLowerCase();  //convertimos el titulo a minusculas
+    titulo=titulo.replace(/\s+/g,'-');  //sustituimos espacios en blanco to "-""
+    titulo=titulo.toLowerCase();        //convertimos el titulo a minusculas
 
     tituloId=quitaEspaciosMayuIdTitle(req.params.postId);
-    console.log("titulo "+titulo);
-      console.log("titulo2 "+item.titulo);
-      
-    console.log("idTitulo "+tituloId);
+    //compara lo que se escribio en el post del navegador sea igual a lo que se escribio en el articulo
+    //Ejemplo:
     if (titulo===tituloId){
-      res.render("post",{newTitle:titulo,
+      res.render("post",{newTitle:item.titulo,
                         newPost:item.contenido});
     }
   });
 });
 
+//Aqui se muestran todas las tareas creadas
 app.get("/home",function(req,res){
   //cleanTitle(array);
    res.render("home",{textoHome:homeStartingContent,newTexto:array
@@ -67,6 +81,7 @@ app.get("/contact",function(req, res){
   res.render("contact",{textoContact:contactContent
   });
 });
+
 
 app.get("/compose",function(req, res){
   res.render("compose");
