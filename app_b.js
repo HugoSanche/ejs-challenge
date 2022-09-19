094081c311562b8c1dd4ada4c01d7544fef2c816
HugoSanche <hbmclaren@gmail.com>
2022-09-18 21:14:25 -0500

    Start saving on mongo db

--- app_b.js
+++ app_b.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+
 //Este proyecto tiene como objetivo crear un diario. Aqui se van guardando el historial de las tareas realizadas
 //De igual manera se tiene como objetivo el uso de plantillas
 
@@ -6,33 +7,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-const mongoose=require("mongoose");
-
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-mongoose.connect("mongodb://localhost:27017/Daily",{useNewUrlParser: true});
-
 //En esta variable se guardan todas las tareas
 //title: el titulo de la tareas
 //contenido: aqui va todo el detalle de la tarea
-const array=
-{
-  titulo: String,
-  contenido:String
-}
-
-
-const PostDaily=mongoose.model("postDaily",array);
-
-const  newCompost={
-    titulo:String,
-    contenido:String
-  };
-
-const Compost=mongoose.model("Compost",newCompost);
-
+let array=[];
 
 let postWithOutSpaces;
 let webPostWithOutSpaces;
@@ -40,6 +22,7 @@ let webPostWithOutSpaces;
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+
 //Permite los templates
 app.set('view engine', 'ejs');
 
@@ -48,35 +31,15 @@ app.use(express.static("public"));
 
 //Cuando se llama a raiz se redirecciona "/home"
 app.get("/",function(req, res){
-  res.redirect("/home");
-});
-
-//Aqui se muestran todas las tareas creadas
-app.get("/home",function(req,res){
-
-  Compost.find(function(err, array){
-    if (array===0){
-      console.log("Registro vacio")
-    }
-    else
-    {
-      res.render("home",{
-        textoHome:array.titulo,
-         newTexto:array
-    });
-    }
-  });
+res.redirect("/home");
 });
 
 //aqui se van agregando todas las tareas
 app.post("/compose", function(req, res){
-
-const newItem=new Compost ({
-    titulo:req.body.title,
+const  newCompost={titulo:req.body.title,
     contenido:req.body.post
-  });
-
-  newItem.save();
+  };
+  array.push(newCompost);
   res.redirect("/home");
 });
 
@@ -86,24 +49,27 @@ const newItem=new Compost ({
 app.get("/post/:postId", function(req, res){
     let tituloId;
     let titulo;
-Compost.find(function(err, array){
-  array.forEach((item) => {
-    titulo=item.titulo;
-    titulo=titulo.replace(/\s+/g,'-');  //sustituimos espacios en blanco to "-""
-    titulo=titulo.toLowerCase();        //convertimos el titulo a minusculas
-    tituloId=quitaEspaciosMayuIdTitle(req.params.postId);
-    //compara lo que se escribio en el post del navegador sea igual a lo que se escribio en el articulo
-    //Ejemplo:
-    if (titulo===tituloId){
-      res.render("post",{newTitle:item.titulo,
-                         newPost:item.contenido});
-                       }
-  });
-})
 
+    array.forEach((item) => {
+      titulo=item.titulo;
+      titulo=titulo.replace(/\s+/g,'-');  //sustituimos espacios en blanco to "-""
+      titulo=titulo.toLowerCase();        //convertimos el titulo a minusculas
+      tituloId=quitaEspaciosMayuIdTitle(req.params.postId);   
+      //compara lo que se escribio en el post del navegador sea igual a lo que se escribio en el articulo
+      //Ejemplo:
+      if (titulo===tituloId){
+        res.render("post",{newTitle:item.titulo,
+                           newPost:item.contenido});
+                         }
+    });
 });
 
-
+//Aqui se muestran todas las tareas creadas
+app.get("/home",function(req,res){
+  //cleanTitle(array);
+   res.render("home",{textoHome:homeStartingContent,newTexto:array
+ });
+});
 
 app.get("/about",function(req, res){
   res.render("about",{textoAbout:aboutContent
